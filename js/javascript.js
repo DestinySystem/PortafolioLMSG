e7abd1bbbf59fdf353236bbc0cfc1069dac79288
DestinySystem <121401354+DestinySystem@users.noreply.github.com>
2024-03-08 09:59:20 +0100

    Update javascript.js
    Updating js

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,18 @@
 //let ventanaDesc=window.open("./main/index.html","Sobre mí",height=500,width=400,resizeable=1);
 
+let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=600,height=300,left=100,top=100`;
 
 function AbrirVentana(obj,location) {
     let ventana = window.open();
     ventana.location = "hello.com/dummypage.html"; //how to assign the url to the newly opened window
 }
-
+button.onclick = () => {
+  window.open("/", "test", params);
+};
 let ventanaDesc=AbrirVentana(ventana,"");
+
+
+
+open("/", "test", params);
 
